5c5d70ed6de504fa34434694db39aad823114065
Matti Lankinen <m.lankinen@iki.fi>
2017-12-03 17:28:03 +0200

    Add RxJS 5.5.2 as a foreign lib

---
+++ src/deps.cljs
@@ -0,0 +1,7 @@
+{:foreign-libs
+ [{:file           "Rx.js"
+   :file-min       "Rx.min.js"
+   :provides       ["cljsjs.rxjs" "rxjs"]
+   :global-exports {rxjs Rx cljsjs.rxjs Rx}}]
+ :externs
+ ["externs.rxjs.js"]}
