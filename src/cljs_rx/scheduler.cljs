e677ece88c75986616e0203b36ef479464414319
Matti Lankinen <m.lankinen@iki.fi>
2017-12-04 12:24:00 +0200

    Add schedulers

---
+++ src/cljs_rx/scheduler.cljs
@@ -0,0 +1,33 @@
+(ns cljs-rx.scheduler
+  (:require [cljsjs.rxjs :as rxjs]
+            [cljs-rx.internal.interrop :refer [fn-2]]))
+
+(def ^:private schedulers
+  (js->clj rxjs/Scheduler :keywordize-keys true))
+
+; === classes ===
+
+(def VirtualTimeScheduler rxjs/VirtualTimeScheduler)
+
+(def TestScheduler rxjs/TestScheduler)
+
+
+; === factories ===
+
+(defn test-scheduler [deep-equals-assertion]
+  (TestScheduler. (fn-2 deep-equals-assertion)))
+
+; TODO: implement virtual time scheduler
+#_(defn virtual-time-scheduler []
+    (VirtualTimeScheduler.))
+
+
+; === globals ===
+
+(def asap (:asap schedulers))
+
+(def async (:async schedulers))
+
+(def animation-frame (:animationFrame schedulers))
+
+(def queue (:queue schedulers))
